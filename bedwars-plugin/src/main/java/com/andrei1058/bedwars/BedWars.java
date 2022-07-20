158490775c965935ed76e3a5de0843a0fa560aa4
Jaden Bluman <109678443+mceryt@users.noreply.github.com>
2022-07-20 21:30:41 +0530

    Update BedWars.java

--- bedwars-plugin/src/main/java/com/andrei1058/bedwars/BedWars.java
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/BedWars.java
@@ -176,16 +176,6 @@ public class BedWars extends JavaPlugin {
 
         // Setup languages
         new English();
-        new Romanian();
-        new Italian();
-        new Polish();
-        new Spanish();
-        new Russian();
-        new Bangla();
-        new Persian();
-        new Hindi();
-        new Indonesia();
-        new Portuguese();
 
         config = new MainConfig(this, "config");
 
